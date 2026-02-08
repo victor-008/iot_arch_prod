faef33abb4f13ad4f8f0639bdee5b98357fdd67b
rono-victor <ronovictor40@gmail.com>
2026-02-08 11:35:31 +0300

    REST API > historical datapoints

---
+++ backend/app/api/routes.py
@@ -0,0 +1,21 @@
+"""
+REST API for historical data endpoint
+
+"""
+from fastapi import APIRouter
+from app.database.db import cursor
+
+router = APIRouter()
+
+@router.get("/history")
+def history():
+    cursor.execute("SELECT * FROM sensor_data ORDER BY id DESC LIMIT 100")
+    rows = cursor.fetchall()
+
+    return[
+        {
+        "timestamp" : r[1],
+        "temperature" : r[2],
+        "humidity" : r[3]
+        } for r in rows      
+    ]
