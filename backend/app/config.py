d8033c1c9bc0c1a999d66406b7e41113b5322685
rono-victor <ronovictor40@gmail.com>
2026-02-09 08:09:14 +0300

    configurations

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,3 +10,14 @@
 
 # MQTT_BROKER = os.getenv("MQTT_BROKER", "127.0.0.1")
 # MQTT_TOPIC = "sensors"
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+REDIS_HOST = os.getenv("REDIS_HOST")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+JWT_SECRET = os.getenv("JWT_SECRET")
